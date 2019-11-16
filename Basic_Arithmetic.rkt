1c765ab85a05212d46dacd9f158c0efb55e6a264
SebastianRegalado <51178949+SebastianRegalado@users.noreply.github.com>
2019-11-16 00:06:15 -0500

    Update Basic_Arithmetic.rkt

--- Basic_Arithmetic.rkt
+++ Basic_Arithmetic.rkt
@@ -46,3 +46,24 @@
      (* midterm-weight midterm-g)
      (* final-weight final-g)
      (* assign-weight assign-g)))
+     
+;; (tetris-score c-level lines) produces the number of scored points  
+;;   in tetris given c-level and lines
+;; tetris-score: Nat Nat -> Nat
+;; Example:
+(check-expect (tetris-score 19 6) 0)
+
+(define (tetris-score current-level lines)
+  (cond
+    [(>= lines 5) 0]
+    [else (* (+ 1 current-level)
+             (cond [(= lines 1) 40]
+                   [(= lines 2) 100]
+                   [(= lines 3) 300]
+                   [else 1200]))]))
+
+;;Tests
+(check-expect (tetris-score 12 1) 520)
+(check-expect (tetris-score 7 2) 800)
+(check-expect (tetris-score 5 3) 1800)
+(check-expect (tetris-score 9 4) 12000)
